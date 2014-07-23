fbf1428848b08dec30f4652b45eee3fdc48352de
Stephen Young <me@hownowstephen.com>
2014-07-23 13:35:00 -0700

    added collection for linux cpu

---
+++ plugins/cpu/collect_linux.go
@@ -0,0 +1,43 @@
+package cpu
+
+import (
+    "time"
+    "unsafe"
+    "strconv"
+)
+
+func (c *CPU) collect() {
+    c.lastUpdate = time.Now()
+    c.previous = c.current
+
+    file, err := os.Open("/proc/stat")
+    if err != nil {
+        return
+    }
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        f := regexp.MustCompile("\\s+").Split(scanner.Text(), -1)
+
+        is_cpu, err := regexp.MatchString("^cpu\\d*", f[0])
+        if err == nil && is_cpu {
+
+            c.current = map[string]int{
+                "user":   int(strconv.ParseUint(f[1], 10, 64)),
+                "nice":   int(strconv.ParseUint(f[2], 10, 64)),
+                "system": int(strconv.ParseUint(f[3], 10, 64)),
+                "idle":   int(strconv.ParseUint(f[4]], 10, 64)),
+            }
+            break
+        }
+    }
+
+    if c.previous == nil {
+        c.previous = c.current
+    }
+
+    c.previousTotal = c.currentTotal
+    c.currentTotal = c.current["user"] + c.current["nice"] + c.current["system"] + c.current["idle"]
+}
+
+
