d6cb46b60abe52ad6340bc00aebf8d4b18ea0a6f
Stephen Young <me@hownowstephen.com>
2014-08-01 14:24:44 -0700

    updating regex to match spaces properly in foundationdb plugin, gofmt

--- plugins/foundationdb/collect.go
+++ plugins/foundationdb/collect.go
@@ -1,67 +1,70 @@
 package foundationdb
 
 import (
+	"fmt"
+	"net"
+	"os"
 	"os/exec"
 	"regexp"
-	"fmt"
 	"strconv"
-	"os"
-	"net"
 )
 
 // 127.0.0.1:4689         (  1% cpu;   6% machine; 0.000 Gbps;  0% disk IO; 2.4 GB / 11.1 GB RAM  )
 // [ip]      [port]         [cpu]   [machine]      [gbps]     [diskio]   [ram_usage] [ram_total]
-var detail_regex = regexp.MustCompile(`([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}):([0-9]+)[ \(]+ ([0-9]+)% cpu; +([0-9]+)% machine; ([0-9\.]+) Gbps; +([0-9]+)% disk IO; +([0-9\.]+) GB +/ +([0-9\.]+)`)
+var detail_regex = regexp.MustCompile(`([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}):([0-9]+)[ \(]+ ([0-9]+)% cpu; *([0-9]+)% machine; *([0-9\.]+) Gbps; *([0-9]+)% disk IO; *([0-9\.]+) GB +/ *([0-9\.]+)`)
 var workload_regex = regexp.MustCompile(` +([a-zA-Z]+) rate +- +([0-9]+) Hz`)
 
 type machine struct {
-	ip string
-	port int
-	cpu int
-	machine int
-	gbps float64
-	diskio int
-	ram_used float64
+	ip        string
+	port      int
+	cpu       int
+	machine   int
+	gbps      float64
+	diskio    int
+	ram_used  float64
 	ram_total float64
 }
 
 func (f *FoundationDB) collect() {
 
-    usage, err := exec.Command("fdbcli", "--exec", "status details").Output()
-    if err != nil {
-        fmt.Println(err)
-    }
+	usage, err := exec.Command("fdbcli", "--exec", "status details").Output()
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    // Extract cluster information
-    for _, matched := range workload_regex.FindAllSubmatch(usage, -1) {
-    	hz := bytesToFloat64(matched[2])
-    	switch string(matched[1]) {
-    		case "Read": f.read_rate = hz
-    		case "Write": f.write_rate = hz
-    		case "Transaction": f.transaction_rate = hz
-    		case "Conflict": f.conflict_rate = hz
-    	}
-    }
+	// Extract cluster information
+	for _, matched := range workload_regex.FindAllSubmatch(usage, -1) {
+		hz := bytesToFloat64(matched[2])
+		switch string(matched[1]) {
+		case "Read":
+			f.read_rate = hz
+		case "Write":
+			f.write_rate = hz
+		case "Transaction":
+			f.transaction_rate = hz
+		case "Conflict":
+			f.conflict_rate = hz
+		}
+	}
 
-    // Extract machine-specific information
-    ips := myIps()
-    for _, matched := range detail_regex.FindAllSubmatch(usage, -1) {
+	// Extract machine-specific information
+	ips := myIps()
+	for _, matched := range detail_regex.FindAllSubmatch(usage, -1) {
 
-    	if ips[string(matched[1])] && bytesToInt(matched[2]) == f.port {
-    		
-    		f.cpu = bytesToInt(matched[3])
-    		f.traffic = bytesToFloat64(matched[5])
-    		f.diskio = bytesToInt(matched[6])
-    		f.ram_used = bytesToFloat64(matched[7])
-    		f.ram_total = bytesToFloat64(matched[8])
-    		
-    		return
+		if ips[string(matched[1])] && bytesToInt(matched[2]) == f.port {
 
-    	}
-    }
+			f.cpu = bytesToInt(matched[3])
+			f.traffic = bytesToFloat64(matched[5])
+			f.diskio = bytesToInt(matched[6])
+			f.ram_used = bytesToFloat64(matched[7])
+			f.ram_total = bytesToFloat64(matched[8])
+
+			return
+
+		}
+	}
 
 }
-
 
 func myIps() map[string]bool {
 
@@ -75,7 +78,7 @@ func myIps() map[string]bool {
 	}
 
 	addrs, err := net.LookupHost(name)
-		if err != nil {
+	if err != nil {
 		fmt.Printf("Oops: %v\n", err)
 		return output
 	}
@@ -86,7 +89,6 @@ func myIps() map[string]bool {
 
 	return output
 }
-
 
 func bytesToInt(b []byte) int {
 	s := string(b)
